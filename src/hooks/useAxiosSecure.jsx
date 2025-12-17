4287abfdc1b63082edda35c225d814e568077292
Md. Shoyaif Rahman <sohan0020p@gmail.com>
2025-12-17 22:39:41 +0600

    polish

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router";
 import { useEffect } from "react";
 
 const axiosSecure = axios.create({
-    baseURL:'http://localhost:3000'
+    baseURL:'https://localchefbazar-iota.vercel.app'
 })
 
 const useAxiosSecure = () => {
